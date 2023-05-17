fb13ba754b3ee41bee82a8b62e39d453c9164f3f
seojinwoong <tjwlsdnd12@naver.com>
2023-05-17 15:03:45 +0900

    회원가입 완성

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import SignUpPage from './components/SignUpPage/SignUpPage';
 import LoginPage from './components/LoginPage/LoginPage';
+import ChatPage from './components/ChatPage/ChatPage';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { useDispatch, useSelector } from 'react-redux';
+import { setUser, clearUser } from './redux/actions/user_action';
+import Loader from './utils/components/Loader';
 
 const App = () => {
+  const auth = getAuth();
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const isLoading = useSelector(state => state.user_reducer.isLoading);
 
-  return (
-    <Routes>
-      <Route path="/" element={<SignUpPage />}></Route>
-      <Route path="/login" element={<LoginPage />}></Route>
-    </Routes>
-  )
+  useEffect(() => {
+    onAuthStateChanged(auth, user => {
+      if (user) {
+        navigate('/');
+        dispatch(setUser(user));
+      } else {
+        navigate('/login');
+        dispatch(clearUser());
+      }
+    })
+  }, []);
+
+  if (isLoading) {
+    return (
+      <Loader />
+    )
+  } else {
+    return (
+      <Routes>
+        <Route path="/" element={<ChatPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    )
+  }
 }
 
 export default App
--- src/components/SignUpPage/SignUpPage.js
+++ src/components/SignUpPage/SignUpPage.js
@@ -3,13 +3,74 @@ import './SignUpPage.css';
 import { IoIosWarning } from 'react-icons/io';
 import defaultProfile from '../../utils/images/default_profile.png';
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import Spinner from 'react-bootstrap/Spinner';
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { getDatabase, ref, set } from 'firebase/database';
+import { getStorage, ref as strRef, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
 
 const SignUpPage = () => {
+  const [submitLoading, setSubmitLoading] = useState(false);
+  const [uploadLoading, setUploadLoading] = useState(false);
+  const [uploadedImgUrl, setUploadedImgUrl] = useState('');
 
+  const { register, watch, formState: { errors }, handleSubmit } = useForm({mode: 'onChange'});
   const uploadImgRef = useRef();
+  const password = useRef();
+  password.current = watch('password');
   
   const handleUploadImgClick = () => {
     uploadImgRef.current.click();
+  }
+
+  const handleUploadProfile = async (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const metadata = { contentType: file.type };
+      const storage = getStorage();
+  
+      setUploadLoading(true);
+  
+      try {
+        const uploadTask = uploadBytesResumable(strRef(storage, `profileUpload/${file.name}`), file, metadata);
+        getDownloadURL(uploadTask.snapshot.ref).then(downloadURL => {
+          setUploadedImgUrl(downloadURL);
+        });
+        setUploadLoading(false);
+  
+      } catch (error) {
+        alert('프로필 이미지 업로드 도중 오류가 발생하였습니다!');
+        console.error(error.message);
+        setUploadLoading(false);
+  
+      }
+    } else {
+      setUploadedImgUrl("");
+    }
+  }
+
+  const onSubmit = async (data) => {
+    try {
+      setSubmitLoading(true);
+
+      const auth = getAuth();
+      const createdUser = await createUserWithEmailAndPassword(auth, data.email, data.password);
+      let updateInfo = { displayName: data.name };
+      if (uploadedImgUrl) updateInfo.photoURL = uploadedImgUrl
+
+      await updateProfile(auth.currentUser, updateInfo);
+      set(ref(getDatabase(), `/users/${createdUser.user.uid}`), {
+        name: createdUser.user.displayName,
+        image: createdUser.user.photoURL,
+      });
+
+      setSubmitLoading(false);
+
+    } catch (error) {
+      alert('회원가입 도중 오류가 발생하였습니다!');
+      console.error(error.message);
+      setSubmitLoading(false);
+    }
   }
 
   return (
@@ -17,25 +78,47 @@ const SignUpPage = () => {
       <div className='page-container'>
           <div className='inner'>
             <h2 className='title'>회원가입</h2>
-            <form>
-              <input type="text" className='common-inputs mb-3' placeholder='이메일'/>
-              <p className='warning'><IoIosWarning /> 에베베</p>
-              <input type="text" className='common-inputs mb-3' placeholder='이름'/>
-              <input type="password" className='common-inputs mb-3' placeholder='패스워드'/>
-              <input type="password" className='common-inputs mb-4' placeholder='패스워드 확인'/>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <input type="email" className='common-inputs mb-3' placeholder='이메일'
+                name="email" { ...register('email', { required: true, pattern: /^\S+@\S+$/i }) }
+              />
+              { errors.email && errors.email.type === 'required' && <p className='warning'><IoIosWarning /> 이메일은 필수입력 값입니다.</p>}
+              { errors.email && errors.email.type === 'pattern' && <p className='warning'><IoIosWarning /> 유효하지 않은 이메일 형식입니다.</p>}
 
-              {/*  upload-user-profile-wrap start */}
+              <input type="text" className='common-inputs mb-3' placeholder='이름'
+                name="name" { ...register('name', { required: true, maxLength: 10 }) }
+              />
+              { errors.name && errors.name.type === 'required' && <p className='warning'><IoIosWarning /> 이름은 필수입력 값입니다.</p>}
+              { errors.name && errors.name.type === 'maxLength' && <p className='warning'><IoIosWarning /> 이름은 최대 10자까지 입력 가능합니다.</p>}
+
+              <input type="password" className='common-inputs mb-3' placeholder='비밀번호'
+                name="password" { ...register('password', { required: true, minLength: 6 }) }
+              />
+              { errors.password && errors.password.type === 'required' && <p className='warning'><IoIosWarning /> 비밀번호는 필수입력 값입니다.</p>}
+              { errors.password && errors.password.type === 'minLength' && <p className='warning'><IoIosWarning /> 비밀번호는 최소 6자 이상 입력하셔야 합니다.</p>}
+
+              <input type="password" className='common-inputs mb-4' placeholder='비밀번호 확인'
+                name="password_confirm" { ...register('password_confirm', {required: true, validate: value => value === password.current}) }
+              />
+              { errors.password_confirm && errors.password_confirm.type === 'required' && <p className='warning'><IoIosWarning /> 비밀번호 확인은 필수입력 값입니다.</p>}
+              { errors.password_confirm && errors.password_confirm.type === 'validate' && <p className='warning'><IoIosWarning /> 비밀번호가 일치하지 않습니다.</p>}
+
               <div className='upload-user-profile-wrap'>
                 <p>프로필 이미지</p>
                 <div className='profile-row'>
-                  <img className='profile-img' src={defaultProfile} alt="프로필 이미지" />
-                  <button type="button" className='common-button upload-btn' onClick={handleUploadImgClick}>사진 업로드</button>
-                  <input type="file" accept='images/jpeg, image/png' ref={uploadImgRef} />
+                  <img className='profile-img' alt="프로필 이미지" 
+                    src={uploadedImgUrl !== "" ? uploadedImgUrl : defaultProfile}
+                  />
+                  <button disabled={uploadLoading} type="button" className='common-button upload-btn' onClick={handleUploadImgClick}>
+                    {uploadLoading ? <Spinner animation="border" variant="light" /> : '사진 업로드'}
+                  </button>
+                  <input type="file" accept='images/jpeg, image/png' ref={uploadImgRef} onChange={handleUploadProfile}/>
                 </div>
               </div>
-              {/*  upload-user-profile-wrap end */}
 
-              <button className='common-button signup-btn mb-2'>제출</button>
+              <button disabled={submitLoading} className='common-button signup-btn mb-2'>
+                { submitLoading ? <Spinner animation="border" variant="light" /> : '제출' }
+              </button>
               <div className='go-link-wrap mb-4'>
                 <p className='question'>이미 아이디가 있으신가요?</p>
                 <Link to="/login" className='go-link'>로그인하러 가기</Link>
