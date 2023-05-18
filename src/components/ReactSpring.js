aba9e27649914a6fe99bf1ea87045fd98abff40b
seojinwoong <tjwlsdnd12@naver.com>
2023-05-18 11:28:55 +0900

    react-spring 개념정리2

--- src/components/ReactSpring.js
+++ src/components/ReactSpring.js
@@ -3,17 +3,26 @@ import { useTransition, animated } from 'react-spring';
 import '../../src/tester.css'
 
 const ReactSpring = () => {
-  const [isVisible, setIsvisible] = useState(false);
-  const transition = useTransition(isVisible, {
-    from: { x: -100, y: 800, opacity: 0 },
-    enter: { x: 0, y: 0, opacity: 1 },
+  const [items, setItems] = useState([]);
+  const transition = useTransition(items, {
+    from: { x: -100, y: 800, opacity: 0, width: 10, height: 10 },
+    enter: item => async (next) => {
+      await next({ y: item.y, opacity: 1, delay: item.delay });
+      await next({ x: 0, width: 100, height: 100 });
+    },
     leave: { x: 100, y: 800, opacity: 0 }
   })
 
   return (
     <div id='tester'>
-      <button onClick={() => {setIsvisible(v => !v)}}>
-        {isVisible ? 'un-mount' : 'mount'}    
+      <button onClick={() => {
+        setItems(v => v.length ? [] : [
+          { y: -100, delay: 200 },
+          { y: -50, delay: 400 },
+          { y: 0, delay: 600 }
+        ])
+      }}>
+        {items.length ? 'un-mount': 'mount'}
       </button>
       <div className='container'>
           {transition((style, item) => 
